052721caa0dfb821aa4fd7b6ee813652d986a707
Anshi Wang <anshiwang16@gmail.com>
2026-09-15 16:19:22 -0400

    initial commit

---
+++ training.py
@@ -0,0 +1,28 @@
+import tensorflow   as tf
+
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+model = MobileNetV2(weights = "imagenet", include_top=False)
+model.trainable = False
+
+#load photos and assigns them a marker based on class
+train_ds = tf.keras.utils.image_dataset_from_directory(r"C:\Users\anshi\Desktop\garbage sorting\data\Raw Data\Training Dataset", image_size=(224, 224), batch_size = 32)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(r"C:\Users\anshi\Desktop\garbage sorting\data\Raw Data\Validation Dataset", image_size=(224, 224), batch_size = 32)
+
+#processes the photos to adapt them to the model requirements
+train_ds = train_ds.map(lambda x, y: (preprocess_input(x), y))
+
+val_ds = val_ds.map(lambda x, y: (preprocess_input(x), y))
+
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+
+full_model = Sequential([
+    model,
+    GlobalAveragePooling2D(),
+    Dense(4, activation='softmax')
+])
+
