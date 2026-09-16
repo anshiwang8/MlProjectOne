f3563e47b86ed1067e6e22e43700d5d3b1a86ea8
Anshi Wang <anshiwang16@gmail.com>
2026-09-16 16:25:46 -0400

    final training commit

--- training.py
+++ training.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-h
+
 model = MobileNetV2(weights = "imagenet", include_top=False)
 model.trainable = False
 
